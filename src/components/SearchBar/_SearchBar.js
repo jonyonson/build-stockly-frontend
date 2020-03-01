8850a18712767e4d5aef90296a607770c0d53c5f
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-03-01 09:51:28 -0600

    update prettier

--- src/components/SearchBar/_SearchBar.js
+++ src/components/SearchBar/_SearchBar.js
@@ -6,7 +6,7 @@ import { makeSearchSelection } from '../../actions';
 import './SearchBar.scss';
 
 // Use your imagination to render suggestions.
-const renderSuggestion = suggestion => {
+const renderSuggestion = (suggestion) => {
   // console.log('suggestion', suggestion);
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -25,7 +25,7 @@ function SearchBar({ makeSearchSelection }) {
   useEffect(() => {
     if (matches.length > 0) {
       const matchesToDisplay = matches.filter(
-        match => !match['1. symbol'].includes('.')
+        (match) => !match['1. symbol'].includes('.'),
       );
 
       setSuggestions(matchesToDisplay);
@@ -35,10 +35,10 @@ function SearchBar({ makeSearchSelection }) {
   // When suggestion is clicked, Autosuggest needs to populate the input
   // based on the clicked suggestion. Teach Autosuggest how to calculate the
   // input value for every given suggestion.
-  const getSuggestionValue = selection => {
+  const getSuggestionValue = (selection) => {
     const suggestion = {
       symbol: selection['1. symbol'],
-      name: selection['2. name']
+      name: selection['2. name'],
     };
     makeSearchSelection(suggestion);
     return `${suggestion.symbol}, ${suggestion.name}`;
@@ -53,21 +53,21 @@ function SearchBar({ makeSearchSelection }) {
   // Autosuggest will call this function every time you need to clear suggestions.
   const onSuggestionsClearRequested = () => setSuggestions([]);
 
-  const fetchBestMatches = input => {
+  const fetchBestMatches = (input) => {
     const API_KEY = process.env.REACT_APP_API_KEY;
     const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${input}&apikey=${API_KEY}`;
 
     axios
       .get(url)
-      .then(res => setMatches(res.data.bestMatches))
-      .catch(err => console.error(err));
+      .then((res) => setMatches(res.data.bestMatches))
+      .catch((err) => console.error(err));
   };
 
   const inputProps = {
     placeholder: 'Search by symbol or name',
     type: 'search',
     value,
-    onChange
+    onChange,
   };
 
   return (
@@ -84,5 +84,5 @@ function SearchBar({ makeSearchSelection }) {
 
 export default connect(
   null,
-  { makeSearchSelection }
+  { makeSearchSelection },
 )(SearchBar);
